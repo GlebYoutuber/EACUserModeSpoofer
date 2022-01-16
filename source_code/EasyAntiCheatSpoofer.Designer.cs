c57708c9e67c0aa02e04af7ff19a43568fcba4b3
GlebYoutuber <73891760+GlebYoutuber@users.noreply.github.com>
2022-01-16 21:00:02 +0200

    Add files via upload

---
+++ source_code/EasyAntiCheatSpoofer.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace EACUserModeSpoofer
+{
+    partial class EasyAntiCheatSpoofer
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(137, 382);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 62);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Spoof Your PC!!!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Grand Aventure Shadow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label1.Location = new System.Drawing.Point(22, 159);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(604, 63);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome to My new Spoofer for Online Multiplayer Games(EAC Spoofer)\r\n\r\nEnjoy!!!";
+            // 
+            // EasyAntiCheatSpoofer
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(656, 496);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Constantia", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "EasyAntiCheatSpoofer";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Easy Anti Cheat UserMode Spoofer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ source_code/EasyAntiCheatSpoofer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EACSpoofer.HWID.Spoof;
+using EACSpoofer.HWID.GetHWID;
+using EACSpoofer.MacAddressSpoof;
+using EACSpoofer.VolumeIDSpoofer;
+using System.IO;
+using System.Reflection;
+
+namespace EACUserModeSpoofer
+{
+    public partial class EasyAntiCheatSpoofer : Form
+    {
+        public static void Extract(string nameSpace, string outDirectory, string internalFilePath, string resourceName)
+        {
+            //This is Very Important Code... DON'T CHANGE THIS!!! 
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+
+            using (Stream s = assembly.GetManifestResourceStream(nameSpace + "." + (internalFilePath == "" ? "" : internalFilePath + ".") + resourceName))
+            using (BinaryReader r = new BinaryReader(s))
+            using (FileStream fs = new FileStream(outDirectory + "\\" + resourceName, FileMode.OpenOrCreate))
+            using (BinaryWriter w = new BinaryWriter(fs))
+                w.Write(r.ReadBytes((int)s.Length));
+        }
+        public EasyAntiCheatSpoofer()
+        {
+            InitializeComponent();
+            //Show MessageBox!!!
+            MessageBox.Show("WARNING: THIS IS SPOOFER FOR AVOID HWID BAN(HARDWARE ID), BE CAREFUL TO USE THIS!!!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            Directory.CreateDirectory(@"C:\Temp");
+            Extract("EACUserModeSpoofer", @"C:\Temp", "Resources", "Volumeid.exe");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you Want Spoofing your PC???", "EAC UserMode Spoofer", MessageBoxButtons.YesNo, MessageBoxIcon.Hand, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+                MACAddressSpoof macspoof = new MACAddressSpoof();
+                macspoof.SpoofMACAddress();
+                Spoofer xzspoof = new Spoofer();
+                xzspoof.SpoofHWID();
+                VolumeIDSpoofer idspoofer = new VolumeIDSpoofer();
+                idspoofer.VolumeIDSpoof();
+            }
+            if(MessageBox.Show("Do you want to show your HWID?!", "EAC UserMode Spoofer", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
+            {
+                GetHWID hwidshow = new GetHWID();
+                hwidshow.ShowHWID();
+                File.WriteAllText(@"C:\Temp\Readme.txt", "Hi, thank you for using this EAC UserMode Spoofer for Avoiding HWID Ban(EasyAntiCheat.exe is collect information about your Registry Keys" + Environment.NewLine + "EasyAntiCheat.sys is protecting anyone game from Third Party Programs), Made by GlebYoutuber");
+                Process.Start("notepad", @"C:\Temp\Readme.txt");
+            }
+            MessageBox.Show("Spoofing your PC is Completed, Automatic Restart is Begin...");
+            Process.Start("shutdown", "/r /t 120");
+            Environment.Exit(-1254);
+        }
+    }
+}
